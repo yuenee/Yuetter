727bee91beabea2faf61e1f24bdd92ce94cdc9a1
yuenee <61732983+yuenee@users.noreply.github.com>
2022-07-23 15:32:59 +0900

    first commit

---
+++ src/main/java/com/example/yuetter/YuetterApplication.java
@@ -0,0 +1,13 @@
+package com.example.yuetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuetterApplication.class, args);
+	}
+
+}
