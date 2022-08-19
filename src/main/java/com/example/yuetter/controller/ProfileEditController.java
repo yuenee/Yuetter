e4d7b98b5dd32d6b72ffffed66b3de4abf54bf76
Shogoohashi <syougo.work9139@gmail.com>
2022-08-19 22:05:27 +0900

    プロフィール編集画面のデザインが完成いたしました。ご確認の程、よろしくお願いいたします。

--- src/main/java/com/example/yuetter/controller/ProfileEditController.java
+++ src/main/java/com/example/yuetter/controller/ProfileEditController.java
@@ -4,10 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class ProfileEditController {
 
-    @GetMapping("/home")
-    public String getHome() {
-        return "home/home";
+
+    @GetMapping("/profileEdit")
+    public String getProfileEdit() {
+        return "home/profileEdit";
     }
 }
+
